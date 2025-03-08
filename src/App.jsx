6221d8d352900d342c9c02ef1282cee7eba7acf0
Rajit jain <noddysinghai053@gmail.com>
2025-03-09 01:08:48 +0530

    added new Readme.md file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, HashRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { Toaster } from 'react-hot-toast';
 import Home from './pages/Home';
@@ -8,7 +8,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Router>  {/* Replaced HashRouter with BrowserRouter */}
+    <HashRouter>
       <AuthProvider>
         <Toaster position="top-right" />
         <Navbar />
@@ -19,7 +19,7 @@ function App() {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </AuthProvider>
-    </Router>
+      </HashRouter> //if the repository name is not coming at the end of the link then add hashrouter.
   );
 }
 
